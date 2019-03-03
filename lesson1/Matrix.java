52e645c59952f82f7e6132b66f802f3c8fdbc4a1
Ekaterina999999 <ekaaterina99@gmail.com>
2019-03-03 12:18:53 +0300

    Add lesson 1

---
+++ lesson1/Matrix.java
@@ -0,0 +1,12 @@
+public class Matrix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm working");
+		System.out.println("Reverse: ");
+		for( int i = args.length - 1 ; i >= 0 ; i--)
+            System.out.print(args[i]+"  ");
+
+}
+	
+}
